229edb8ee511ac7cb2b8510704141172cc4e9abd
lakshyagarg911 <69689875+lakshyagarg911@users.noreply.github.com>
2020-11-14 19:43:50 +0530

    Leaderboards! (#1)

---
+++ leaderboard.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def send(name, score):
+    try:
+        Req = requests.get("http://cb-leaderboard.herokuapp.com/",
+                           params={'game': 'physics', 'name': name, 'score': score})
+        req = Req.json()
+        if req['state'] == 'success':
+            return 'success'
+    except Exception as e:
+        return 'failed'
+
+
+def receive():
+    data = requests.get("http://cb-leaderboard.herokuapp.com/get", params={'game': 'physics'})
+    req = data.json()
+    to_return = []
+    print(len(req))
+    if len(req) > 10:
+        arg_2 = 11
+    else:
+        arg_2 = len(req) + 1
+    for x in range(1, arg_2):
+        to_return.append(req[str(x)])
+    return to_return
